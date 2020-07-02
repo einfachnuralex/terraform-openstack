b1474b319a16c3f08905e40c4cfc44a34c3df444
Raphael Grömmer <r.groemmer@yahoo.de>
2020-07-02 09:29:06 +0000

    reconfigured kubeadm config and script

--- config/kubeadm-init.sh
+++ config/kubeadm-init.sh
@@ -6,9 +6,9 @@ do
 done
 
 set -e
-sudo kubeadm init --config kubeadmconfig.yaml --upload-certs --v=5 | tee result.log
-grep -B2 \\--certificate-key result.log > master_join.sh
-tail -n 2 result.log > worker_join.sh
+sudo kubeadm init --config kubeadmconfig.yaml --v=5 | tee result.log
+sudo kubeadm --print-join-command > worker_join.sh
+(cat worker_join.sh ; echo '--control-plane')
 
 %{ for master_ip in master_ips ~}
 ssh-keyscan -H ${master_ip} >> ~/.ssh/known_hosts
