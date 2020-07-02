9d0e1651c85239e1a4cb3817c8f905d6052a8009
Raphael Grömmer <r.groemmer@yahoo.de>
2020-07-02 09:30:25 +0000

    removed nginx lb

--- instances.tf
+++ instances.tf
@@ -1,27 +1,4 @@
 # Instance creation
-resource "openstack_compute_instance_v2" "loadbalancer" {
-  name      = var.lb_name
-  flavor_id = data.openstack_compute_flavor_v2.flavor.id
-  key_pair  = var.key_pair_name
-
-  block_device {
-    uuid                  = var.image_id
-    source_type           = "image"
-    destination_type      = "volume"
-    boot_index            = 0
-    volume_size           = 20
-    delete_on_termination = true
-  }
-
-  network {
-    port = openstack_networking_port_v2.lb_port_v4.id
-  }
-
-  /* network {
-    port = openstack_networking_port_v2.lb_port_v6.id
-  } */
-}
-
 resource "openstack_compute_instance_v2" "master_nodes" {
   for_each  = var.master_node_names
   name      = each.key
